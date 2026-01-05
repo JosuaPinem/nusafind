2750bcd97cbd1739ac85fbc6c76bfd361142dcd0
Josua Pinem <josuapinem@nusa.net.id>
2026-01-05 11:10:36 +0700

    Updat

--- src/service/ask.py
+++ src/service/ask.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify
 from model.model import getRawData, createAnswer, fillterQuestion, saveData, Vis
 from db.connection import create_local_connection
-import mysql.connector
+import mysql.connector, time
 
 class askService:
     def ask_service(self, question, session_id):
@@ -20,19 +20,37 @@ class askService:
         if rawData is None:
             return None, None, None
         print("masuk visualisasi")
+        start_vis = time.perf_counter()
+
         getchart, vis = Vis(rawData)
-        print("keluar visualisasi")
+
+        end_vis = time.perf_counter()
+        print(f"keluar visualisasi (durasi: {end_vis - start_vis:.4f} detik)")
+
         print("masuk create jawaban")
+        start_create = time.perf_counter()
+
         answer = createAnswer(rawData, fillter, question)
-        print("keluar create jawaban")
+
+        end_create = time.perf_counter()
+        print(f"keluar create jawaban (durasi: {end_create - start_create:.4f} detik)")
+
         if answer is None:
             return None, None, None
+
         print("masuk simpan data")
+        start_save = time.perf_counter()
+
         save = saveData(session_id, question, fillter, rawData, answer, vis)
-        print("keluar simpan data")
+
+        end_save = time.perf_counter()
+        print(f"keluar simpan data (durasi: {end_save - start_save:.4f} detik)")
+
         if save is False:
             return "Mohon maaf data tidak berhasil disimpan.", "", ""
+
         print(answer)
+
         return answer, fillter, getchart
     
     def getChatHis(self, session_id):
